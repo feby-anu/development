fec486da55b04ac7ca5e4b2fc4e2b84f317486c3
feby-anu <febychandy24@gmail.com>
2025-02-25 17:56:19 +0000

    change in the code

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask
-app=Flask(__name__)
+
+app = Flask(__name__)
 
 @app.route('/')
-def index():
-    return 'Hello Feby how are you doing !!!!!'
+def hello():
+    return "Hello Feby ,All good!!!!"
 
-app.run(debug=True)
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
